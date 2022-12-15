652ae34ed410675838af8d8a68edea60ab763c84
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-15 15:39:29 +0100

    Fix 1 for task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,14 +6,9 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		free(tree);
+	if (!tree)
 		return;
-	}
-	if (!(tree->left))
-		binary_tree_delete(tree->left);
-	if (!(tree->right))
-		binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
