27d7bb5c52769e4af761d6bf28046cb468a4edaf
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-15 21:07:52 +0100

    Added file for task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,33 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Insert binary tree node to the left child
- * @parent: Pointer to the parent node of the binary tree
- * @value: Value to be stored in the binary tree
- *
+ * binary_tree_sibling - Return node sibling
+ * @node: Pointer to the node of the binary tree
  * Return: Pointer to the new node or NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *node;
-
-	/* Check if parent exists */
-	if (!parent)
+	/* Check if node exists */
+	if (!node || !(node->parent))
 		return (NULL);
-	/* Check if memory can be allocated */
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-		return (NULL);
-	/* Set the values */
-	node->parent = parent;
-	node->n = value;
-	node->right = NULL;
-	node->left = parent->left;
-	/* Check if parent has existing left-child node and change parent */
-	if ((parent->left) != NULL)
-		parent->left->parent = node;
-	/* Set the parent left-child to new node */
-	parent->left = node;
-
-	return (node);
+	/* Check if node is left-child node or right-child and return sib */
+	if ((node->parent->left) == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
