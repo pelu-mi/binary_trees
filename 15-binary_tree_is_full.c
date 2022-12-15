425213f2400d0425b5396d979d805651f3f25e82
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-15 20:39:31 +0100

    Fix 1 for task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,27 @@
 #include "binary_trees.h"
 
 /* Helper Function */
-int _is_full(const binary_tree_t *tree);
+int is_full(const binary_tree_t *tree);
+
+/**
+ * is_full - Count number of nodes with one child
+ * @tree: pointer to tree root
+ * Return: Number of nodes with 1 child
+ */
+int is_full(const binary_tree_t *tree)
+{
+	int full = 0;
+
+	/* Add 1 if any node exists with one child */
+	if (!((tree->left && tree->right) || (!(tree->left) && !(tree->right))))
+		full += 1;
+	/* Traverse the tree and count */
+	if (tree->left)
+		full += is_full(tree->left);
+	if (tree->right)
+		full += is_full(tree->right);
+	return (full);
+}
 
 /**
  * binary_tree_is_full - Check if the tree is full
@@ -13,25 +33,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	/* Check if tree exists */
 	if (!tree)
 		return (0);
-	return ((_is_full(tree) > 0) ? 0 : 1);
-}
-
-/*
- * _is_full - Check and count number of nodes with one child
- * @tree: pointer to tree root
- * Return: Number of nodes with 1 child
- */
-int _is_full(const binary_tree_t *tree)
-{
-	int full = 0;
-
-	/* Add 1 if any node exists with one child */
-	if (!((tree->left && tree->right) || (!(tree->left) && !(tree->right))))
-		full += 1;
-	/* Traverse the tree and count */
-	if (tree->left)
-		full += _is_full(tree->left);
-	if (tree->right)
-		full += _is_full(tree->right);
-	return (full);
+	return ((is_full(tree) > 0) ? 0 : 1);
 }
