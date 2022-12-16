3c6b7c53fd23a3a428991a0a976c0dc74bfc9d92
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-16 01:36:26 +0100

    Minor change

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Rotate binary tree left
+ * @tree: Pointer to root node
+ * Return: pointer to the new root node
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	return (NULL);
+}
